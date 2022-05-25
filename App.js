59e61f424180002c111c387ea421d5a80afaf71e
Clayton Tabossi de Melo <ctabossi@gmail.com>
2022-05-25 18:28:25 -0300

    Novo layout de cores

--- App.js
+++ App.js
@@ -76,12 +76,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#1E1240'}]}>
           <Text style={[styles.textButton, { color: 'white', fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'black' : '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, { color: typeof (button) === 'number' ? '#FCFCFC' : '#746A7E'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -98,17 +98,17 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#1E1240',
   },
   resultText: {
-    color: '#282F38',
+    color: '#FCFCFC',
     fontSize: 32,
     fontWeight: 'bold',
     padding: 12,
     textAlign: 'right',
   },
   historyText:{
-    color: '#7c7c7c',
+    color: '#746A7E',
     fontSize: 20,
     marginRight: 10,
     alignSelf: 'flex-end',
@@ -118,7 +118,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#3D0075',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
     flex: 2,
   },
   textButton: {
-    color: '#7c7c7c',
+    color: '#746A7E',
     fontSize: 20,
   }, 
 });
